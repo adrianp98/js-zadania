6281f45c557dd369a5566b25baebe821f399b0cc
AdikLBN <adrian.pietrasiuk98@gmail.com>
2024-03-16 21:00:18 +0100

    poprawa formatowania

--- js-suma-zmiennych/js/main.js
+++ js-suma-zmiennych/js/main.js
@@ -2,11 +2,10 @@ let firstNumber = 10;
 
 let secondNumber = 20;
 
-function add (num1, num2) {
+function add(num1, num2) {
+  let result = num1 + num2;
 
-    let result = num1 + num2
-
-    return result
+  return result;
 }
 
-console.log(add (firstNumber, secondNumber));
+console.log(add(firstNumber, secondNumber));
